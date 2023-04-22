acbfc509bb078344552fb6c4d7106795be7373e4
c-diego <diegocorte02@hotmail.com>
2023-04-22 03:08:40 -0300

    configuring the project with spring initializr

---
+++ src/main/java/br/edu/utfpr/td/tsi/application/BoapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/utfpr/td/tsi/application/BoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
